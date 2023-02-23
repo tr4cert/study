c0d9ec664825747d50b1f8d4131ceb545ace860e
tttracert <tttraceroute@gmail.com>
2023-02-23 16:04:25 +0300

    first

--- src/HomeWork.java
+++ src/HomeWork.java
@@ -12,10 +12,6 @@ import java.util.Scanner;
 // Отправить в чат задания ссылку на pull-request
 public class HomeWork {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter you full name");
-        String fullName = sc.nextLine();
-        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
-        System.out.println("Program result: " + encodedFullName);
+        System.out.println("hello");
     }
 }
