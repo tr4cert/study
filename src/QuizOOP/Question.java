ac14d0d6c38901adf0aac9d6f1e601a84f3f2a98
tttracert <tttraceroute@gmail.com>
2023-02-28 21:49:55 +0300

    QuizOOP structure changed

--- src/QuizOOP/Question.java
+++ src/QuizOOP/Question.java
@@ -1,20 +1,19 @@
 package QuizOOP;
 
 public class Question {
-    private static int score = 0;
-    private static int questionsAmount = 0;
+
 
     private final int NUMBER;
     private final String TEXT;
     private final Option[] OPTIONS;
 
+
     public Question(int number, String text, Option[] options) {
         this.NUMBER = number;
         this.TEXT = text;
         this.OPTIONS = options;
-
-        questionsAmount++;
     }
+
 
     public void printQuestion() {
         System.out.println(NUMBER + ". " + TEXT);
@@ -23,17 +22,12 @@ public class Question {
         }
     }
 
+
     public boolean isAnswerCorrect(int answerNumber) {
         if (answerNumber >= 1 && answerNumber <= OPTIONS.length) {
-            if (this.OPTIONS[answerNumber - 1].IS_CORRECT_OPTION) {
-                score++;
-                return true;
-            }
+            return this.OPTIONS[answerNumber - 1].IS_CORRECT_OPTION;
         }
         return false;
     }
 
-    public static String getResult() {
-        return "Результат: " + score + " правильных ответов из " + questionsAmount;
-    }
 }
---
+++ src/QuizOOP/Test.java
@@ -0,0 +1,78 @@
+package QuizOOP;
+
+import java.util.Scanner;
+
+public class Test {
+    private static int score = 0;
+    private static int questionsAmount;
+    private final Question[] questions;
+
+    public Test() {
+        this.questions = generateQuestions();
+        questionsAmount = questions.length;
+    }
+
+
+    public void passTest() {
+        for (Question question : questions) {
+            question.printQuestion();
+            ask(question);
+        }
+
+        printResult();
+    }
+
+
+    private static void ask(Question question) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int userAnswer = sc.nextInt();
+            if (question.isAnswerCorrect(userAnswer)) {
+                System.out.println("Правильно");
+                score++;
+            } else System.out.println("Неправильно");
+        } catch (Exception e) {
+            System.out.println("На ввод подано некорректное значение");
+
+        }
+    }
+
+
+    public static void printResult() {
+        System.out.println("Результат: " + score + " правильных ответов из " + questionsAmount);
+    }
+
+
+    private static Question[] generateQuestions() {
+
+        Question questionFirst = new Question(1, "Какая из этих премий впоследствии была переименована в Государственную премию СССР?", new Option[]{
+                new Option(1, "Сталинская", true),
+                new Option(2, "Ленинская", false),
+                new Option(3, "Ломоносовская", false),
+                new Option(4, "Нобелевская", false),
+                new Option(5, "Дмитриевская", false)});
+
+
+        Question questionSecond = new Question(2, "Какое из перечисленных государств возглавляет королева Великобритании?", new Option[]{
+                new Option(1, "Коста-Рика", false),
+                new Option(2, "Шри-Ланка", false),
+                new Option(3, "Малазия", false),
+                new Option(4, "Ямайка", true)});
+
+
+        Question questionThird = new Question(3, "Какой вид березы славится красивой древесиной?", new Option[]{
+                new Option(1, "Корейская", false),
+                new Option(2, "Карельская", true),
+                new Option(3, "Курильская", false)});
+
+
+        Question questionFourth = new Question(4, "Что разрушали луддиты?", new Option[]{
+                new Option(1, "Католические храмы", false),
+                new Option(2, "Машины", true),
+                new Option(3, "Античные статуи", false),
+                new Option(4, "Школы", false)});
+
+
+        return new Question[]{questionFirst, questionSecond, questionThird, questionFourth};
+    }
+}
